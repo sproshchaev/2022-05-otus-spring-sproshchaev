53e7054d7c2f50143192781aecf3c0708d920756
sproshchaev <sproshchaev@gmail.com>
2022-06-22 14:21:00 +0500

    Init.

---
+++ spring-03-testing-students/src/main/java/ru/otus/spring03testingstudents/Spring03TestingStudentsApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.spring03testingstudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring03TestingStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring03TestingStudentsApplication.class, args);
+	}
+
+}
