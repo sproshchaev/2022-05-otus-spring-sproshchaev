d088313514124d5897d7256a57b413544d5f3303
sproshchaev <sproshchaev@gmail.com>
2022-07-14 16:10:22 +0500

    Init.

---
+++ spring-04-testing-students/src/main/java/ru/otus/Main.java
@@ -0,0 +1,14 @@
+package ru.otus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Class Main started!");
+		SpringApplication.run(Main.class, args);
+	}
+
+}
