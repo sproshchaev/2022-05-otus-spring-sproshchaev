55993926fb06dc2001dd2412f0eedbaa2b1bd44a
sproshchaev <sproshchaev@gmail.com>
2022-07-19 15:10:49 +0500

    init

---
+++ spring-05-books/src/main/java/ru/otus/spring05books/Spring05BooksApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.spring05books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring05BooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring05BooksApplication.class, args);
+	}
+
+}
