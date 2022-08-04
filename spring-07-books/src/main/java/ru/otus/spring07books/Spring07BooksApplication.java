fdb9c269ce952a9dd452545b2822bb115f370180
sproshchaev <sproshchaev@gmail.com>
2022-08-04 12:47:45 +0500

    Init. (start.spring.io)

---
+++ spring-07-books/src/main/java/ru/otus/spring07books/Spring07BooksApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.spring07books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring07BooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring07BooksApplication.class, args);
+	}
+
+}
