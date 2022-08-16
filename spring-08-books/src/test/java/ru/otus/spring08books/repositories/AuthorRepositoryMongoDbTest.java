6012c151555ffba1edff046dff21ea09852541a6
sproshchaev <sproshchaev@gmail.com>
2022-08-16 17:50:41 +0500

    Added test

---
+++ spring-08-books/src/test/java/ru/otus/spring08books/repositories/AuthorRepositoryMongoDbTest.java
@@ -0,0 +1,17 @@
+package ru.otus.spring08books.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataMongoTest
+@ExtendWith(SpringExtension.class)
+class AuthorRepositoryMongoDbTest {
+
+    @Test
+    void findAllByFullName() {
+    }
+}
