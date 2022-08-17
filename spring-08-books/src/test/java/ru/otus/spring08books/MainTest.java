393c89962de97f604d91f1f51de5f555fbf0b7b3
sproshchaev <sproshchaev@gmail.com>
2022-08-17 13:22:49 +0500

    Tests are running!

--- spring-08-books/src/test/java/ru/otus/spring08books/MainTest.java
+++ spring-08-books/src/test/java/ru/otus/spring08books/MainTest.java
@@ -1,11 +1,7 @@
 package ru.otus.spring08books;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MainTest {
