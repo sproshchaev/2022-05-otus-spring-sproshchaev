1e2d7abc1d700c9720e41e05a2e2fe015fe9dcf2
sproshchaev <sproshchaev@gmail.com>
2022-08-23 13:23:01 +0500

    Added repository CommentRepository

---
+++ spring-09-books/src/main/java/ru/otus/spring09books/repositories/CommentRepository.java
@@ -0,0 +1,31 @@
+package ru.otus.spring09books.repositories;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import ru.otus.spring09books.entities.Comment;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Интерфейс CommentRepository содержит методы работы со справочником комментариев
+ *
+ * @see ru.otus.spring09books.entities.Comment
+ */
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> getAllByBookId(long idBook);
+
+    @EntityGraph(attributePaths = {"book"})
+    Optional<Comment> findCommentById(long id);
+
+    @Modifying
+    @Query("update Comment c set c.commentText = :commentText where c.id = :idComment")
+    Integer updateComment(@Param("idComment") long idComment, @Param("commentText") String commentText);
+
+}
