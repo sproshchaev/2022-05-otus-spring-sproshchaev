066760a763ac2ff9e6fff1ba40dac7a6642bd6ad
sproshchaev <sproshchaev@gmail.com>
2022-08-23 13:12:27 +0500

    Added repository Genre

---
+++ spring-09-books/src/main/java/ru/otus/spring09books/repositories/AuthorRepository.java
@@ -0,0 +1,28 @@
+package ru.otus.spring09books.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import ru.otus.spring09books.entities.Author;
+
+import java.util.List;
+
+/**
+ * Интерфейс AuthorRepository содержит методы работы со справочником авторов
+ *
+ * @see ru.otus.spring09books.entities.Author
+ */
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    List<Author> getAuthorByFullName(String fullName);
+
+    @Query("select a.id from Author a where a.fullName = :fullName")
+    List<Long> getAuthorIdByFullName(@Param("fullName") String fullName);
+
+    @Modifying
+    @Query("update Author a set a.fullName = :fullName where a.id = :id")
+    Integer updateAuthor(@Param("id") long id, @Param("fullName") String fullName);
+}
