8df2a1f2c0e28ccf1d57fd34b608dbbe9d6936c0
sproshchaev <sproshchaev@gmail.com>
2022-09-15 17:07:40 +0500

    Init.

---
+++ spring-12-books/src/main/resources/db/migration/data/R__0005_Add_user.sql
@@ -0,0 +1,4 @@
+insert into user_access(login, password, fullname)
+values ('reader', '123456', 'John Doe'),
+       ('librarian', '1234567', 'Jane Harper'),
+       ('administrator', '12345678', 'Sylvia Mendez');
---
+++ spring-12-books/src/test/java/ru/otus/spring12books/Main.java
@@ -0,0 +1,19 @@
+package ru.otus.spring12books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Приложение "Библиотека" на Spring Security, Spring Web, Thymeleaf, Spring Data JPA, MS SQL Server, Flyway
+ *
+ * @author Sergey Proshchaev
+ * @version 1.0
+ */
+@SpringBootTest
+class Main {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
