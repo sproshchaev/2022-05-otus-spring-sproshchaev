2586cb89e7d8021029e1164517102e7926505657
sproshchaev <sproshchaev@gmail.com>
2022-09-15 21:48:42 +0500

    Updated

---
+++ spring-12-books/src/main/java/ru/otus/spring12books/Main.java
@@ -0,0 +1,19 @@
+package ru.otus.spring12books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Приложение "Библиотека" на Spring Security, Spring Web, Thymeleaf, Spring Data JPA, MS SQL Server, Flyway
+ *
+ * @author Sergey Proshchaev
+ * @version 1.0
+ */
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
