fb97c980bc34ba3b61ad0d5b4ef5a5b3301eab91
sproshchaev <sproshchaev@gmail.com>
2022-09-21 15:35:10 +0500

    Init.

---
+++ spring-13-books/src/main/java/ru/otus/spring13books/Spring13BooksApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.spring13books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring13BooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring13BooksApplication.class, args);
+	}
+
+}
---
+++ spring-13-books/src/test/java/ru/otus/spring13books/Spring13BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.spring13books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring13BooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
