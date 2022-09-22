09c5e9b88d4279957d785a4157ec9452c9113a1a
sproshchaev <sproshchaev@gmail.com>
2022-09-22 15:08:40 +0500

    Updated test

--- spring-13-books/src/test/java/ru/otus/spring13books/MainTests.java
+++ spring-13-books/src/test/java/ru/otus/spring13books/MainTests.java
@@ -2,7 +2,7 @@ package ru.otus.spring13books;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+// todo папка с ресурсами в тестах (настройка подключения к БД) нужна?
 @SpringBootTest
 class MainTests {
 
